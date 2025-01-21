8fb945c680ee8f63aeb2c2e954c840efc73dfd46
Bernardo <b.quintinoguzman@ugto.mx>
2025-01-20 23:24:11 -0600

    styles and responsive in nav and bio

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,27 @@ scrollUp.addEventListener(
         window.scrollTo({top:0, left:0, behavior:"smooth",});
     }
 );
+
+// Nav burger menu selection
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener(
+    "click",
+    ()=>{
+        ul.classList.toggle("show");
+    }
+);
+
+// Close burger menu when a link is clicked.
+// Select nav link
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) => {
+    link.addEventListener(
+        "click", () => { ul.classList.remove("show"); }
+    );
+});
